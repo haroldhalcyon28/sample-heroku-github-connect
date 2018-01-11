b9ffef5e9f1dde45e50a5e971c39c1a9c4b124af
internmpcausing <intern03.halcyondigital@gmail.com>
2018-01-11 19:09:56 +0800

    add json-server and unirest

--- server.js
+++ server.js
@@ -3,9 +3,10 @@
 const express = require('express');
 const socketIO = require('socket.io');
 const path = require('path');
-
+const unirest = require('unirest');
 const PORT = process.env.PORT || 8080;
 const INDEX = path.join(__dirname, 'index.html');
+const DBHOST = 'http://192.168.1.73:3000';
 
 // for changing console to debug and for adding log time
 var DEBUG = (function () {
@@ -52,12 +53,35 @@ io.on('connection', (socket) => {
         });
     });
 
+    socket.on('get-init-data', (data) => {
+        unirest.get('http://localhost:3000/employeeTimeIn').end(
+            response => {
+                io.emit('send-init-rate', response.body);
+                console.log(response.body);
+            }
+        );
+    });
+
+    socket.on('get-init-notif', () => {
+        unirest.get(`${DBHOST}/employeeTimeIn?_page=1&limit=20`).end(
+            response => {
+                io.emit('send-init-notif', response.body);
+                console.log(response.body);
+            }
+        );
+    });
+
+
     socket.on('changeColor', (newColor) => {
         DEBUG.log('New color ' + newColor + ' by ' + socket.id);
         io.emit('new-color', {
             color: newColor,
             id: socket.id
         });
+    });
+
+    socket.on('cl-send-timein', (data) => {
+        io.emit('sv-get-pic', data);
     });
 
     socket.on('disconnect', () => console.log('Client disconnected'));
