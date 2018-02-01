a45c1cae3e19f53b4d16a48466abb5cb487b1680
internmpcausing <intern03.halcyondigital@gmail.com>
2018-02-01 16:22:39 +0800

    m

--- config/database.js
+++ config/database.js
@@ -1,6 +1,6 @@
 module.exports = {
-    // uri: 'mongodb://harold:johnp@ds249707.mlab.com:49707/employee-log-socket-io',   //prod
-    uri: 'mongodb://localhost:27017/mydb',    //dev
+    uri: 'mongodb://harold:johnp@ds249707.mlab.com:49707/employee-log-socket-io',   //prod
+   // uri: 'mongodb://localhost:27017/mydb',    //dev
    // uri: 'mongodb://192.168.1.73:27017/mydb',    //dev
     secret: 'yoursecret'
   }
